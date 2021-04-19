c9086c5fea063e59cfe15875c0fc616c79dd319c
mkn nkm <oyasumi120@gmail.com>
2021-04-19 16:17:23 +0900

    mark up files

---
+++ js/main.js
@@ -0,0 +1,83 @@
+'use strict';
+
+// const price = 150;
+
+// console.log( price * 140 );
+// console.log( price * 160 );
+
+
+// console.log( 5 + 3 );
+// console.log( parseInt('5',10) + 3 );
+
+// console.log( parseInt('hello',10));
+
+/**
+ * 条件演算子
+ */
+// const score = 85;
+// score >=80 ? console.log('Great!') : console.log('OK...');
+
+/**
+ * 論理演算子
+ */
+// &&　なおかつ(AND)
+// ||　もしくは(OR)
+// !　~ではない(NOT)
+// const score = 80;
+// const name = 'momo';
+// if(score >= 50 && name === 'momo'){
+//   console.log('Good!')
+// }
+
+/**
+ * 条件分岐
+ */
+// const signal = "pink";
+
+// switch(signal){
+//   case 'red':
+//     console.log('stop!');
+//     break;
+//   case 'yellow':
+//     console.log('caution!');
+//     break;
+//   case 'blue':
+//   case 'green':
+//     console.log('go!');
+//     break;
+//   default:
+//     console.log('wrong signal');
+// }
+
+/**
+ * ループ処理
+ */
+// for(let i=1; i<=10; i++){
+//   // console.log('hello');
+//   // console.log('hello' + i);
+//   console.log(`hello ${i}`);
+// }
+
+/**
+ * while処理
+ */
+// let hp = 100;
+
+// while (hp > 0){
+//   console.log(`${hp} HP left!`);
+//   hp -= 15;
+// }
+
+// let hp = -50;
+// do {
+//   console.log(`${hp} HP left!`);
+//   hp -= 15;
+// }while (hp > 0);
+
+/**
+ * continue,break
+ */
+
+for(let i=1; i<=10; i++){
+  console.log(i);
+}
