9eb727d974b532cec8c78a8d3a8d29c3cdbf53ce
mkn nkm <oyasumi120@gmail.com>
2021-04-20 09:05:59 +0900

    追加機能

--- js/main.js
+++ js/main.js
@@ -1,83 +1,41 @@
 'use strict';
 
-// const price = 150;
+const onClickAdd = () => {
+  // テキストボックスの値の取得＆初期化
+  const inputText = document.getElementById("add_text").value;
+  document.getElementById("add_text").value = "";
+  // li~生成
+  const li = document.createElement("li");
+  li.className = "todo_list";
+  const span = document.createElement("span");
+  span.innerText = inputText;
 
-// console.log( price * 140 );
-// console.log( price * 160 );
+  // button
+  const conpleteButton = document.createElement("button");
+  conpleteButton.innerText = "完了";
+  conpleteButton.addEventListener("click" , () => {
+    alert("完了");
+  });
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click" , () => {
+    alert("削除");
+  });
 
 
-// console.log( 5 + 3 );
-// console.log( parseInt('5',10) + 3 );
+  li.appendChild(span);
+  li.appendChild(conpleteButton);
+  li.appendChild(deleteButton);
 
-// console.log( parseInt('hello',10));
+  // 未完了ulに追加
+  document.getElementById("progress_list").appendChild(li);
 
-/**
- * 条件演算子
- */
-// const score = 85;
-// score >=80 ? console.log('Great!') : console.log('OK...');
+  
 
-/**
- * 論理演算子
- */
-// &&　なおかつ(AND)
-// ||　もしくは(OR)
-// !　~ではない(NOT)
-// const score = 80;
-// const name = 'momo';
-// if(score >= 50 && name === 'momo'){
-//   console.log('Good!')
-// }
+};
 
-/**
- * 条件分岐
- */
-// const signal = "pink";
 
-// switch(signal){
-//   case 'red':
-//     console.log('stop!');
-//     break;
-//   case 'yellow':
-//     console.log('caution!');
-//     break;
-//   case 'blue':
-//   case 'green':
-//     console.log('go!');
-//     break;
-//   default:
-//     console.log('wrong signal');
-// }
+document.addEventListener("DOMContentLoaded", function(){
+  document.getElementById("add_button").addEventListener("click", () => onClickAdd());
 
-/**
- * ループ処理
- */
-// for(let i=1; i<=10; i++){
-//   // console.log('hello');
-//   // console.log('hello' + i);
-//   console.log(`hello ${i}`);
-// }
-
-/**
- * while処理
- */
-// let hp = 100;
-
-// while (hp > 0){
-//   console.log(`${hp} HP left!`);
-//   hp -= 15;
-// }
-
-// let hp = -50;
-// do {
-//   console.log(`${hp} HP left!`);
-//   hp -= 15;
-// }while (hp > 0);
-
-/**
- * continue,break
- */
-
-for(let i=1; i<=10; i++){
-  console.log(i);
-}
+}, false);
